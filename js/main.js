a0f641a7cbc989cc4b19246078a59185f169a494
alcasber25 <alcasber25@gmail.com>
2026-04-22 16:40:02 +0200

    inicio de proyecto

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Ejercicio 1: Saludo personalizado
+//Crea una función llamada saludar que acepte un nombre como argumento y devuelva un mensaje de saludo como: "¡Hola, [nombre]!".
+
+
+
+//Ejercicio 2: Suma de dos números
+//Crea una función llamada suma que acepte dos números y devuelva la suma de estos.
+
+//Ejercicio 3: Números pares Escribe una función llamada esPar que reciba un número y devuelva true si el número es par o false si es impar.
+
+//Ejercicio 4: Mayor de tres números Escribe una función llamada mayorDeTres que reciba tres números como argumentos y devuelva el número mayor.
+
+//Ejercicio 5: Conversión de grados Celsius a Fahrenheit
+//Crea una función llamada celsiusAFahrenheit que convierta una temperatura de grados Celsius a Fahrenheit usando la fórmula: F = (C * 9/5) + 32.
